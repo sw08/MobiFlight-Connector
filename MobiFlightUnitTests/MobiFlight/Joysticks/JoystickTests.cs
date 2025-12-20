d3dc2bb7db93662c3f16fb10f25a9441783abd50
Sebastian M <sebastian@mobiflight.com>
2025-12-20 15:23:20 +0100

    Added unit tests

---
+++ MobiFlightUnitTests/MobiFlight/Joysticks/JoystickTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace MobiFlight.Tests
+{
+    [TestClass()]
+    public class JoystickTests
+    {
+        [TestMethod()]
+        public void GetAxisNameForUsage_ShouldOnlyContainMaxNumberOfEntries()
+        {
+            // Arrange - Valid axis usage IDs that map to JoystickState properties
+            var validUsageIds = new List<int>
+            {
+                48, // X
+                49, // Y
+                50, // Z
+                51, // RotationX
+                52, // RotationY
+                53, // RotationZ
+                54, // Slider1
+                55  // Slider2
+            };
+
+            // Arrange - Invalid usage IDs that don't map to any JoystickState property
+            var invalidUsageIds = new List<int>
+            {
+                46, // Below valid range
+                47, // Below valid range
+                56, // Wheel - not supported by JoystickState
+                57  // HatSwitch - not supported by JoystickState (should be POV)
+            };
+
+            // Act & Assert - Valid IDs should return axis names
+            validUsageIds.ForEach(id =>
+            {
+                var axisName = Joystick.GetAxisNameForUsage(id);
+                Assert.IsFalse(string.IsNullOrEmpty(axisName),
+                    $"UsageMap should contain valid usage ID {id} and return a non-empty axis name.");
+            });
+
+            invalidUsageIds.ForEach(id =>
+            {
+                var throws = false;
+                try
+                {
+                    var name = Joystick.GetAxisNameForUsage(id);
+                }
+                catch
+                {
+                    throws = true;
+                }
+                Assert.IsTrue(throws);
+            });
+        }
+
+        [TestMethod()]
+        public void GetAxisNameForUsage_ValidIds_ReturnsExpectedNames()
+        {
+            // Verify exact mappings
+            Assert.AreEqual("X", Joystick.GetAxisNameForUsage(48));
+            Assert.AreEqual("Y", Joystick.GetAxisNameForUsage(49));
+            Assert.AreEqual("Z", Joystick.GetAxisNameForUsage(50));
+            Assert.AreEqual("RotationX", Joystick.GetAxisNameForUsage(51));
+            Assert.AreEqual("RotationY", Joystick.GetAxisNameForUsage(52));
+            Assert.AreEqual("RotationZ", Joystick.GetAxisNameForUsage(53));
+            Assert.AreEqual("Slider1", Joystick.GetAxisNameForUsage(54));
+            Assert.AreEqual("Slider2", Joystick.GetAxisNameForUsage(55));
+        }
+    }
+}
